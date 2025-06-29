25bfa7c6563a786a708d2fec3a5a80109c63bd72
ryanmwai <ckmwai06@gmail.com>
2025-06-29 18:06:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+const numbers = [10, 6, 5, 20];
+const max = Math.max(...numbers);
